4259ed7253666bf051ac667d5ab770c127b36e65
Filip Johansson <FilipJohansson99@protonmail.com>
2019-06-03 09:04:37 +0200

    Added helping text to experimental build

--- CalculatorConsole/Experimental.cs
+++ CalculatorConsole/Experimental.cs
@@ -11,10 +11,12 @@ namespace CalculatorConsole
         public static void ExperimentalMain()
         {
             Console.WriteLine("Enter a question e.g. 10+10, only two variables may be entered");
+            Console.WriteLine("Available Methods\n\"+\",\"-\",\"/\",\"*\"");
 
             var question = "";
             var op = "";
             double sum = 0;
+            start:
             question = Console.ReadLine();
 
             if (question.Contains("+")) 
@@ -40,6 +42,12 @@ namespace CalculatorConsole
                 op = "-";
                 var numbers = question.Split("-");
                 sum = Convert.ToDouble(numbers[0]) - Convert.ToDouble(numbers[1]);
+            }
+            else if (sum == 0)
+            {
+                Console.WriteLine("I did not understand that question.");
+                Console.WriteLine("Please format your question correctly e.g. \"{number} {method} {number}\"");
+                goto start;
             }
             question = question.Replace(" ", $"{null}");
             string result = ($"{question} = {sum}");
